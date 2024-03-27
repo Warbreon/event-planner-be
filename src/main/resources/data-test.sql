fdd95f3404a3e78cf8833eeb3d65f69c76af7d47
Apozzz <bosas.arnas2000@gmail.com>
2024-03-27 18:45:06 +0300

    changes: changed build.gradle for ci/cd

---
+++ src/main/resources/data-test.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (first_name, last_name, job_title, phone_number, email, image_url, password_hash, role) VALUES
+('John', 'Doe', 'Developer', '1234567890', 'user@example.com', null, '$2a$12$gljcIqq.b589M3EPqETCoOUX74kECEPX.cMCUJQqgt1zppLbJcyKm', 'USER'),
+('Jane', 'Doe', 'Event Coordinator', '0987654321', 'admin@example.com', null, '$2a$12$gljcIqq.b589M3EPqETCoOUX74kECEPX.cMCUJQqgt1zppLbJcyKm', 'EVENT_ADMIN'),
+('Jim', 'Beam', 'System Administrator', '1122334455', 'sysadmin@example.com', null, '$2a$12$gljcIqq.b589M3EPqETCoOUX74kECEPX.cMCUJQqgt1zppLbJcyKm', 'SYSTEM_ADMIN');
