25f2228ba941c6e328ac83e2c10651b69732cade
Warbreon <w4rbreon@gmail.com>
2024-04-11 21:07:14 +0300

    added temporary cors config to access data without logging

---
+++ src/main/java/com/cognizant/EventPlanner/config/CorsConfig.java
@@ -0,0 +1,43 @@
+package com.cognizant.EventPlanner.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${cors.allowedOrigins}")
+    private String allowedOrigins;
+
+    @Value("${cors.allowedMethods}")
+    private String allowedMethods;
+
+    @Value("${cors.allowedHeaders}")
+    private String allowedHeaders;
+
+    @Value("${cors.exposedHeaders}")
+    private String exposedHeaders;
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(Arrays.asList(allowedOrigins.split(",")));
+        configuration.setAllowedMethods(Arrays.asList(allowedMethods.split(",")));
+        configuration.setAllowedHeaders(Arrays.asList(allowedHeaders.split(",")));
+        configuration.setExposedHeaders(Arrays.asList(exposedHeaders.split(",")));
+
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return new CorsFilter(source);
+    }
+}
