1857fdeeff36c6dfdd297e0a179972276b482aae
Warbreon <w4rbreon@gmail.com>
2024-04-11 20:42:45 +0300

    removed unnecessary outputs

--- src/test/java/com/cognizant/EventPlanner/EventPlannerApplicationTests.java
+++ src/test/java/com/cognizant/EventPlanner/EventPlannerApplicationTests.java
@@ -1,18 +1,13 @@
 package com.cognizant.EventPlanner;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EventPlannerApplicationTests {
 
-	@Value("${spring.datasource.url}")
-	private String dbUrl;
-
 	@Test
 	void contextLoads() {
-		System.out.println("Database URL: " + dbUrl);
 	}
 
 }
