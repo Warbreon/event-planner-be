49dc34c855428da6756902bcb345b5d6d605c726
Warbreon <w4rbreon@gmail.com>
2024-05-03 13:42:35 +0300

    added caching for tags and addresses

--- src/main/java/com/cognizant/EventPlanner/services/facade/AddressManagementFacade.java
+++ src/main/java/com/cognizant/EventPlanner/services/facade/AddressManagementFacade.java
@@ -2,6 +2,7 @@ package com.cognizant.EventPlanner.services.facade;
 
 import com.cognizant.EventPlanner.services.AddressService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +13,7 @@ public class AddressManagementFacade {
 
     private final AddressService addressService;
 
+    @Cacheable(value = "addresses")
     public List<String> getAllCities() {
         return addressService.findAllCities();
     }
