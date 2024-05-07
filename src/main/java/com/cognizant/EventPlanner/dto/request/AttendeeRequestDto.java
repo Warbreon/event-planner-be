58f751356fc300327ce7605a85bad24d44624bc3
Apozzz <bosas.arnas2000@gmail.com>
2024-05-07 14:35:46 +0300

    fix: removed unneeded changes

--- src/main/java/com/cognizant/EventPlanner/dto/request/AttendeeRequestDto.java
+++ src/main/java/com/cognizant/EventPlanner/dto/request/AttendeeRequestDto.java
@@ -1,6 +1,8 @@
 package com.cognizant.EventPlanner.dto.request;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
